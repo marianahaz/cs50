1451f6c717d2413711e0ea4981e44ab4a8a68196
mariana haz <mariana.haz@hotmail.com>
2020-04-12 19:37:08 -0300

    Create 13.sql

---
+++ pset7/movies/13.sql
@@ -0,0 +1,1 @@
+SELECT DISTINCT name FROM people JOIN stars ON id = person_id WHERE movie_id IN (SELECT movie_id FROM people JOIN stars ON id = person_id WHERE name = 'Kevin Bacon' AND birth = 1958) AND name != 'Kevin Bacon';
